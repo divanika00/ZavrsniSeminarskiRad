e125129e73c9c360e8015a5cf1bf398a2d41ca44
Domagoj <102039831+divanika00@users.noreply.github.com>
2022-08-16 20:13:08 +0200

    Delete README.md

---
+++ Data/Migrations/20230902212957_Initial2.0.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZavrsniSeminarskiRad.Data.Migrations
+{
+    public partial class Initial20 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Order");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ShoppingBasketId = table.Column<int>(type: "int", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Paid = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Order_ShoppingBasket_ShoppingBasketId",
+                        column: x => x.ShoppingBasketId,
+                        principalTable: "ShoppingBasket",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_ShoppingBasketId",
+                table: "Order",
+                column: "ShoppingBasketId");
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,31 @@
+link za GuitHub:
+
+https://github.com/divanika00/ZavrsniSeminarskiRad.git
+---------------------------------------------
+
+Admin login :
+Email = "admintest@test.com",
+UserName = "admintest@test.com",
+
+
+admintest@test.com
+Pa$$word321
+
+---------------------------------------------
+Employee login:
+Email = "employeetest@test.com",
+UserName = "employeetest@test.com",
+
+
+employeetest@test.com
+Pa$$word321
+
+---------------------------------------------
+BasicUser login: 
+
+Email = "usertest@test.com",
+UserName = "usertest@test.com",
+
+
+usertest@test.com
+Pa$$w0rd
